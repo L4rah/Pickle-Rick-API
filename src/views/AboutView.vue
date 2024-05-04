359d7b70e774f7f4398e804fbcba803a3df2d484
Larah Valentini Mallavazi <127799837+L4rah@users.noreply.github.com>
2024-05-03 22:21:37 -0300

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 offset-md-3 text-center">
-        <img src="https://img.r7.com/images/r7rio-082023-macaco-flamengo-18082023084350267?dimensions=771x420&no_crop=true" alt="Profile picture" class="rounded-circle mb-3" width="150">
+        <img src="https://likemark.files.wordpress.com/2010/01/photo.jpg" alt="Profile picture" class="rounded-circle mb-3" width="300">
         <h1 class="display-4">Larah Valentini Mallavazi</h1>
         <p class="lead"><strong>Universidade de Marília - UNIMAR</strong></p>
         <p class="lead"><strong>Ánalise e Desenvolvimento de Sistemas</strong></p>
